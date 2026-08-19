e6a687140f33b0158f49a2e01730a083f7455ab6
Caelvanost <caelvanost@gmail.com>
2026-08-19 18:30:15 +0200

    Remove UDP transport facade implementation

--- src/UdpTransport.h
+++ src/UdpTransport.h
@@ -1,28 +1,10 @@
 #pragma once
 
-#include "PCH.h"
+#include "STRPMTransport.h"
 
 namespace OStimTogether
 {
-    // Compatibility facade retained temporarily so existing addon code paths
-    // do not need to change in the same commit. It performs NO UDP/network
-    // work: all sends are delegated exclusively to STRPMTransport.
-    class UdpTransport
-    {
-    public:
-        static UdpTransport& GetSingleton();
-
-        bool Start();
-        void Stop();
-        void Send(std::string_view payload);
-
-        [[nodiscard]] bool IsRunning() const noexcept
-        {
-            return _running.load();
-        }
-
-    private:
-        UdpTransport() = default;
-        std::atomic_bool _running{ false };
-    };
+    // Temporary source-compatibility alias for AddonBridge.cpp.
+    // There is no UDP backend or fallback on the strpm branch.
+    using UdpTransport = STRPMTransport;
 }
