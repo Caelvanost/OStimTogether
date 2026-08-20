547d3ebc1de8165dccc042fc9cc4de561decfb84
Caelvanost <caelvanost@gmail.com>
2026-08-20 23:20:53 +0200

    Use validated OStim interface exchange for addon repair

--- src/AddonStateRepair.cpp
+++ src/AddonStateRepair.cpp
@@ -34,7 +34,7 @@ namespace OStimTogether
             OStim::InterfaceExchangeMessage::MESSAGE_TYPE,
             &exchange,
             sizeof(exchange),
-            "OStim");
+            nullptr);
 
         if (!dispatched || !exchange.interfaceMap) {
             SKSE::log::warn(
