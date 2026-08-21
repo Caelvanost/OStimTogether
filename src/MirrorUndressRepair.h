7f18b6ca8de7c19f1e0334d9be54708219e8499f
Caelvanost <caelvanost@gmail.com>
2026-08-21 12:07:34 +0200

    Track and restore residual mirror armor

--- src/MirrorUndressRepair.h
+++ src/MirrorUndressRepair.h
@@ -19,12 +19,31 @@ namespace OStimTogether
             void listen(OStim::Thread* thread) override;
         };
 
+        class StopListener final : public OStim::ThreadEventListener
+        {
+        public:
+            void listen(OStim::Thread* thread) override;
+        };
+
+        struct ResidualSnapshot
+        {
+            RE::FormID actorFormID{ 0 };
+            std::vector<RE::FormID> items;
+        };
+
         MirrorUndressRepair() = default;
         void HandleStart(OStim::Thread* thread);
+        void HandleStop(OStim::Thread* thread);
         void Repair(std::int32_t threadID);
+        void ForceResidualUnequip(std::int32_t threadID);
+        void RestoreResidual(std::int32_t threadID);
 
         OStim::ThreadInterface* _threads{ nullptr };
         StartListener _startListener;
+        StopListener _stopListener;
         std::atomic_bool _initialized{ false };
+
+        std::mutex _mutex;
+        std::unordered_map<std::int32_t, ResidualSnapshot> _residualByThread;
     };
 }
