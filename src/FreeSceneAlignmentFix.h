80cdab5f50df30f5ef2eb4cb215e10107daa6754
Caelvanost <caelvanost@gmail.com>
2026-08-22 03:28:54 +0200

    Handle free-scene node transitions

--- src/FreeSceneAlignmentFix.h
+++ src/FreeSceneAlignmentFix.h
@@ -21,11 +21,37 @@ namespace OStimTogether
             void listen(OStim::Thread* thread) override;
         };
 
+        class NodeListener final : public OStim::ThreadEventListener
+        {
+        public:
+            void listen(OStim::Thread* thread) override;
+        };
+
+        class StopListener final : public OStim::ThreadEventListener
+        {
+        public:
+            void listen(OStim::Thread* thread) override;
+        };
+
         void HandleStart(OStim::Thread* thread);
-        void ReleaseFreeSceneProxy(std::int32_t threadID);
+        void HandleNode(OStim::Thread* thread);
+        void HandleStop(OStim::Thread* thread);
+        void ScheduleFreeSceneRelease(
+            std::int32_t threadID,
+            std::chrono::milliseconds delay,
+            std::string reason);
+        void ReleaseFreeSceneProxy(
+            std::int32_t threadID,
+            std::string_view reason);
 
         OStim::ThreadInterface* _threads{ nullptr };
         StartListener _startListener;
+        NodeListener _nodeListener;
+        StopListener _stopListener;
         std::atomic_bool _initialized{ false };
+
+        std::mutex _stateMutex;
+        std::unordered_set<std::int32_t> _pendingRelease;
+        std::unordered_set<std::int32_t> _releasedThreads;
     };
 }
